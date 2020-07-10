613c763b0c0fc288e0d5238fdf27da53bc6cd204
Mikhail-Trubin <mikhail.r.trubin@gmail.com>
2020-07-10 12:29:57 +0700

    [+]add book base list page and developer info  commit

---
+++ app/src/main/java/com/example/slproject/MyBookScreen.kt
@@ -0,0 +1,11 @@
+package com.example.slproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyBookScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_book_screen)
+    }
+}
--- app/src/main/java/com/example/slproject/five.kt
+++ app/src/main/java/com/example/slproject/five.kt
@@ -1,11 +1,17 @@
 package com.example.slproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class five : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_five)
+    }
+    fun mybook (view: View){
+        val randomIntent = Intent( this, MyBookScreen::class.java)
+        startActivity(randomIntent)
     }
 }
